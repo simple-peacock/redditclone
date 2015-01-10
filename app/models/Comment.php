823c9ee9c5e992e332c3eca1469ddd456e5b7517
Peter <peter@simplepeacock.com>
2015-01-10 19:44:18 +1100

    Modified Post and Comment models to include a relationship.

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -1,3 +1,11 @@
 <?php
 
-class Comment extends Eloquent {}
+class Comment extends Eloquent {
+
+  // many-to-one relationship with the Post model
+  public function post()
+  {
+    return $this->belongsTo('Post');
+  }
+  
+}
