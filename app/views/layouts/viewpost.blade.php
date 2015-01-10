97edcb726df8da77f307e5b21d046cf4c1175079
Peter <peter@simplepeacock.com>
2015-01-10 19:59:14 +1100

    Added the ability to view comments on each post.

--- app/views/layouts/viewpost.blade.php
+++ app/views/layouts/viewpost.blade.php
@@ -5,7 +5,19 @@
     
 	@include('partials.post', array('post' => $post))
 
-	show all comments
+	<div>
+    	<h3>Comments</h3>
+    	@if (count($post->comments) === 0)
+      		<p>No comments yet on this post.</p>
+    	@else
+      		@foreach ($post->comments as $comment)
+        		<div>
+          			<!-- <p><strong>{{ $comment->name }} says...</strong></p> -->
+          			<blockquote>{{ $comment->comment }}</blockquote>
+        		</div>
+      		@endforeach
+    	@endif
+  	</div>
 	
 	<h3>New Comment</h3>
   	
