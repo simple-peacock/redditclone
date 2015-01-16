0753ce6b2515ed56d9311ca29990002538cd50bb
Peter <peter@simplepeacock.com>
2015-01-16 21:53:48 +1100

    Added title in the navbar rather than as a heading.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,18 +9,18 @@
 <body>
 <div class="uk-container uk-container-center uk-margin-top">
 	
-	<nav class="uk-navbar uk-margin-bottom">
+	<nav class="uk-navbar uk-margin-large-bottom">
+		<a href="" class="uk-navbar-brand">Reddit Clone in Laravel</a>
     	<ul class="uk-navbar-nav">
-        	<li class="uk-active"><a href="">Home</a></li>
+        	<li><a href="">Home</a></li>
         	<li><a href="">About</a></li>
-        	<li class="uk-parent"><a href="">Login</a></li>
+        	<li><a href="">Login</a></li>
     	</ul>
 	</nav>
 
 <div class="uk-grid" data-uk-grid-margin>
 
 <div class="uk-width-medium-3-4">
-	<h1 class="uk-heading-large">Reddit Clone in Laravel</h1>
 	@yield('main-content')
 </div>
 
