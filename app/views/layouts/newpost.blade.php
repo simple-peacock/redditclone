f511815f857a1f990417367fc24120c574478503
Peter <peter@simplepeacock.com>
2015-01-26 23:05:52 +1100

    Modified the new post view to include tabs for posint a link or a text post.

--- app/views/layouts/newpost.blade.php
+++ app/views/layouts/newpost.blade.php
@@ -3,16 +3,49 @@
 @section('main-content')
 
   	<h3>New Post</h3>
-  	<form action="{{ URL::route('createPost') }}" method="post" class="uk-form">
-    	<div class="uk-form-row">
-      		<input class="uk-form-width-large" name="title" type="text" placeholder="Title" />
-    	</div>
-    	<div class="uk-form-row">
-      		<textarea class="uk-form-width-large" rows="10" name="text" placeholder="Write here..."></textarea>
-    	</div>
-    	<div class="uk-form-row">
-    		<button class="uk-button">Submit</button>
-    	</div>
-  	</form>
+  	
+  	<ul class="uk-tab uk-tab-grid" data-uk-tab="{connect:'#tab-content'}">
+  	
+  		<li class="uk-width-1-6"><a href="">Link</a></li>
+  		<li class="uk-width-1-6"><a href="">Text Post</a></li>
+  	
+  	</ul>
+  	
+  	<ul id="tab-content" class="uk-switcher uk-margin">
+  	
+  		<li>
+  		
+  	  	<form action="{{ URL::route('createPost') }}" method="post" class="uk-form">
+    		<div class="uk-form-row">
+      			<input class="uk-width-1-1" name="title" type="text" placeholder="Title" />
+    		</div>
+    		<div class="uk-form-row">
+      			<input class="uk-width-1-1" name="url" type="text" placeholder="URL" />
+    		</div>
+    		<div class="uk-form-row">
+    			<button class="uk-button">Submit</button>
+    		</div>
+  		</form>
+  	
+  		</li>
+  	
+  	
+  		<li>
+  		
+  		<form action="{{ URL::route('createPost') }}" method="post" class="uk-form">
+    		<div class="uk-form-row">
+      			<input class="uk-width-1-1" name="title" type="text" placeholder="Title" />
+    		</div>
+    		<div class="uk-form-row">
+      			<textarea class="uk-width-1-1" rows="10" name="text" placeholder="Write here..."></textarea>
+    		</div>
+    		<div class="uk-form-row">
+    			<button class="uk-button">Submit</button>
+    		</div>
+  		</form>
+  	
+  		</li>
+  	
+  	</ul>
 
 @stop
