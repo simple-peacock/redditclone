fdd775fc3158900853eb1b24ba2fd9a8cafa1e85
Peter <peter@simplepeacock.com>
2015-02-01 22:32:06 +1100

    Moved the comment view into its own separate partial.

--- app/views/layouts/viewpost.blade.php
+++ app/views/layouts/viewpost.blade.php
@@ -9,34 +9,13 @@
     	@if (count($post->comments) === 0)
       		<p>No comments yet on this post.</p>
     	@else
+    		
     		<ul class="uk-comment-list" id="comments">
-      		@foreach ($post->comments as $comment)
-
-      		
-        		<li>
-				
-					<div class="votecol">
-  						<a class="arrow up" href="{{ URL::route('upVoteComment', array('id' => $comment->id)) }}"></a>
-  						<div class="score">{{ $comment->points }}</div>
-  						<a class="arrow down" href="{{ URL::route('downVoteComment', array('id' => $comment->id)) }}"></a>
-					</div>
-        			
-        			<article class="uk-comment">
-        		
-        		    	<header class="uk-comment-header">
-        					<img class="uk-comment-avatar" src="img/placeholder_avatar.svg" alt="">
-        					<div class="uk-comment-title">{{ $comment->user->username }}</div>
-        					<div class="uk-comment-meta">{{ $comment->created_at->diffForHumans() }}</div>
-    					</header>
-          			
-          				<div class="uk-comment-body">{{ $comment->comment }}</div>
-          			
-        			</article>
-        			
-        			
-        		</li>
-      		@endforeach
+      			@foreach ($post->comments as $comment)
+					@include('partials.comment')
+      			@endforeach
       		</ul>
+    	
     	@endif
   	</div>
 	
