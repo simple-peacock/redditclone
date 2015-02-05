b4055b7a69f8147a7c44060a3885563f8da99d47
Peter <peter@simplepeacock.com>
2015-02-05 23:38:11 +1100

    A user must now be authenticated to vote on a post.

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -7,38 +7,38 @@ class PostController extends BaseController {
 	{
       	return View::make('layouts.main-content', array('posts' => Post::orderBy('points','DESC')->paginate(2)));
 	}
-	
-	
-	
+
+
+
 	// create new post with a form
 	public function newPost()
 	{
-		return View::make('layouts.newpost');	
+		return View::make('layouts.newpost');
 	}
-	
-	  
 
-	  
+
+
+
   	public function createPost()
   	{
-    	
+
     	$user = User::findOrFail(Auth::id());
-    	
+
     	$post = new Post();
     	$post->title = Input::get('title');
-    	
+
     	if (Input::has('text')) {
-    		
+
     		$post->text = nl2br(Input::get('text'));
-    		
+
     	} elseif (Input::has('url')) {
-    	
+
     		$post->islink = true;
     		$post->link = Input::get('url');
     	}
-    	
+
     	$post->points = 0;
-    	
+
     	$user->posts()->save($post);
 
     	//return Redirect::route('viewPost', array('id' => $post->id));
@@ -50,35 +50,44 @@ class PostController extends BaseController {
   	// view a post by id
   	public function viewPost($postid)
   	{
-    	
+
     	$post = Post::findOrFail($postid);
-    	
+
 		$comments = $post->comments()->orderBy('points', 'DESC')->get();
 
     	return View::make('layouts.viewpost', array(
       		'post' => $post, 'comments' => $comments
     	));
   	}
-  	
-  	
-  	public function upVotePost($id)
+
+
+		public function upVotePost($id)
   	{
-  		$post = Post::findOrFail($id);
-  		$post->points++;
-  		$post->save();
-  		
+			if(!Auth::check())
+  		{
+
+				return Redirect::route('index')->with('message', 'Please login to vote!');
+
+			} else {
+
+				$post = Post::findOrFail($id);
+				$post->points++;
+				$post->save();
+
+			}
+
   		return Redirect::route('index');
   	}
-  	
+
   	public function downVotePost($id)
   	{
   		$post = Post::findOrFail($id);
   		$post->points--;
   		$post->save();
-  		
+
   		return Redirect::route('index');
   	}
-  	
+
 
 
 }
